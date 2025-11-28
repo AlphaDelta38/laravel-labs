ca13e6830282aca9ede01ab1e3568f0f5de17404
AlphaDelta38 <kiril.morozov.01@gmail.com>
2025-11-28 15:00:13 +0200

    lab 5 has done

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,26 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::command('app:generate-report')
+    ->lastDayOfMonth('23:00')
+    ->before(function () {
+        Log::info('SCHEDULER: Starting monthly report generation...');
+    })
+    
+    ->after(function () {
+        Log::info('SCHEDULER: Monthly report generation attempt completed.');
+    })
+    
+    ->onSuccess(function () {
+        Log::info('SCHEDULER: Monthly report successfully generated!');
+    })
+    
+    ->onFailure(function () {
+        Log::error('SCHEDULER: Error occurred during monthly report generation.');
+    });
